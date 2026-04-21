17481a164b5b2a40442c0df089b38e28c18a1464
luoxiaojun <luoxiaojun1992@sina.cn>
2026-04-21 18:29:34 +0800

    fix(creator): use app.routers for absolute import from /app working dir

--- creator/app/main.py
+++ creator/app/main.py
@@ -10,7 +10,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from routers import blender, file, project
+from app.routers import blender, file, project
 
 
 @asynccontextmanager
